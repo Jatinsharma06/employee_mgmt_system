5dfcbe6264fb8cdc72b22948fe86bfb59940acaa
Jatinsharma06 <143785628+Jatinsharma06@users.noreply.github.com>
2025-02-26 12:40:46 +0530

    Create db.sql

---
+++ db.sql
@@ -0,0 +1,11 @@
+CREATE TABLE employees (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    email VARCHAR(100),
+    phone VARCHAR(15),
+    position VARCHAR(50),
+    department VARCHAR(50),
+    hire_date DATE
+);
+
